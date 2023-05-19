45c5075d8186f88e757ed922363ed1491359d0ec
Sooryadharshini <A00303192@student.ait.ie>
2023-05-19 12:13:28 +0100

    Added annotation to Application class

--- microservice/src/main/java/com/ericsson/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/ericsson/microservice/MicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.ericsson.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(value = "package com.ericsson.entity.SecuritySummary.class")
 @SpringBootApplication
+@EntityScan({ "com.ericsson.entity" })
 public class MicroserviceApplication {
     public static void main(String[] args) {
 
