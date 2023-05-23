64b526d6fd972d410df7b0ce526592b0cad89e92
Carrick-pre <64624685+Carrick-pre@users.noreply.github.com>
2023-05-23 16:42:15 +0100

    added some stuff that helps with the graph and the rating.

--- microservice/src/main/resources/public/script.js
+++ microservice/src/main/resources/public/script.js
@@ -9,6 +9,54 @@
         countElement.append("Count: " + JSON.stringify(count));
     }
 
+    function Drawgraph(json){
+
+    }
+
+    function getAndDrawOverAllRating() {
+        $.ajax({
+            url: "http://localhost:8080/api/v1/rating",
+            type: "GET",
+            success: function(response) {
+                console.log("GET request successful:", response);
+                var ratingOverAllElement = document.getElementById("ratingOverAll");
+                if (response === 1) {
+                    ratingOverAllElement.textContent = "A";
+                } else if (response === 2) {
+                    ratingOverAllElement.textContent = "B";
+                } else if (response === 3) {
+                    ratingOverAllElement.textContent = "C";
+                } else if (response === 4) {
+                    ratingOverAllElement.textContent = "D";
+                } else if (response === 5) {
+                    ratingOverAllElement.textContent = "E";
+                } else {
+                    ratingOverAllElement.textContent = "Unknown";
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log("GET request error:", error);
+                // Handle the error here
+            }
+        });
+    }
+
+
+    function getGraphingData(){
+        $.ajax({
+            url: "http://localhost:8080/api/v1/repo",
+            type: "GET",
+            success: function(response) {
+                console.log("GET request successful:", response);
+                Drawgraph(response);
+            },
+            error: function(xhr, status, error) {
+                console.log("GET request error:", error);
+                // Handle the error here
+            }
+        });
+    }
+
     function getGeneric(number){
         $.ajax({
             url: "http://localhost:8080/api/v1/owasp"+number,
@@ -29,4 +77,8 @@
         for(i =1; i <=10; i++ ){
             getGeneric(i);
         }
+
+        getGraphingData();
+        getAndDrawOverAllRating();
+
 });
