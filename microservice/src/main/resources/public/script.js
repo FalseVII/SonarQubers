d79553a74c5023c81af7599eefcf4da42c8bb218
charlotte <charlottekefeletswe0@gmail.com>
2023-05-24 14:45:52 +0100

    the graph

--- microservice/src/main/resources/public/script.js
+++ microservice/src/main/resources/public/script.js
@@ -61,8 +61,129 @@ function handle(number,list){
 
 }
 
-    function Drawgraph(json){
+    function Drawgraph(){
+    var programData =[];
+    const data = [
+                  	{
+                  		"id": 652,
+                  		"proj_name": "Test",
+                  		"datetime": "2023-05-23T14:44:55.42571",
+                  		"owasp1_count": "0",
+                  		"owasp2_count": "3",
+                  		"owasp3_count": "6",
+                  		"owasp4_count": "0",
+                  		"owasp5_count": "11",
+                  		"owasp6_count": "0",
+                  		"owasp7_count": "1",
+                  		"owasp8_count": "2",
+                  		"owasp9_count": "0",
+                  		"owasp10_count": "0",
+                  		"cest_count": "0",
+                  		"cwe_count": "0"
+                  	},
+                  	{
+                  		"id": 602,
+                  		"proj_name": "Test",
+                  		"datetime": "2023-05-23T11:47:07.628701",
+                  		"owasp1_count": "0",
+                  		"owasp2_count": "3",
+                  		"owasp3_count": "6",
+                  		"owasp4_count": "0",
+                  		"owasp5_count": "11",
+                  		"owasp6_count": "0",
+                  		"owasp7_count": "1",
+                  		"owasp8_count": "2",
+                  		"owasp9_count": "0",
+                  		"owasp10_count": "0",
+                  		"cest_count": "0",
+                  		"cwe_count": "0"
+                  	},
+                  	{
+                  		"id": 552,
+                  		"proj_name": "Test",
+                  		"datetime": "2023-05-23T11:18:42.494881",
+                  		"owasp1_count": "0",
+                  		"owasp2_count": "3",
+                  		"owasp3_count": "6",
+                  		"owasp4_count": "0",
+                  		"owasp5_count": "0",
+                  		"owasp6_count": "0",
+                  		"owasp7_count": "1",
+                  		"owasp8_count": "2",
+                  		"owasp9_count": "0",
+                  		"owasp10_count": "0",
+                  		"cest_count": "0",
+                  		"cwe_count": "0"
+                  	},
+                  	{
+                  		"id": 502,
+                  		"proj_name": "Test",
+                  		"datetime": "2023-05-23T11:16:59.168523",
+                  		"owasp1_count": "0",
+                  		"owasp2_count": "0",
+                  		"owasp3_count": "0",
+                  		"owasp4_count": "0",
+                  		"owasp5_count": "0",
+                  		"owasp6_count": "0",
+                  		"owasp7_count": "0",
+                  		"owasp8_count": "0",
+                  		"owasp9_count": "0",
+                  		"owasp10_count": "0",
+                  		"cest_count": "0",
+                  		"cwe_count": "0"
+                  	},
+                  	{
+                  		"id": 452,
+                  		"proj_name": "Test",
+                  		"datetime": "2023-05-23T10:36:20.013964",
+                  		"owasp1_count": "0",
+                  		"owasp2_count": "0",
+                  		"owasp3_count": "1",
+                  		"owasp4_count": "0",
+                  		"owasp5_count": "0",
+                  		"owasp6_count": "1",
+                  		"owasp7_count": "0",
+                  		"owasp8_count": "0",
+                  		"owasp9_count": "0",
+                  		"owasp10_count": "0",
+                  		"cest_count": "0",
+                  		"cwe_count": "27"
+                  	}
+                  ]
+ for (var i = 0; i < data.length; i++) {
+		const date = new Date(data[i].datetime);
+		const formattedDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+		const allHotSpots =  parseInt(data[i].owasp1_count)+  parseInt(data[i].owasp2_count)+  parseInt(data[i].owasp3_count)
+		+ parseInt(data[i].owasp4_count) +  parseInt(data[i].owasp5_count);
+      programData.push({
+        year: formattedDate,
+        quantity: allHotSpots
+      });
+    }
+          // Create the chart
+            var ctx = document.getElementById('myChartLine').getContext('2d');
+    var myChart = new Chart(ctx, {
 
+      type: 'line',
+      data: {
+        labels: programData.map(function(date) {
+          return date.year;
+        }),
+        datasets: [{
+          label: 'Vulnerabilities',
+          data: programData.map(function(hotspot) {
+            return hotspot.quantity;
+          }),
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false
+      }
+          });
+			return data;
     }
 
     function getAndDrawOverAllRating() {
@@ -100,7 +221,7 @@ function handle(number,list){
             type: "GET",
             success: function(response) {
                 console.log("GET request successful:", response);
-                Drawgraph(response);
+                //Drawgraph(response);
             },
             error: function(xhr, status, error) {
                 console.log("GET request error:", error);
@@ -130,7 +251,8 @@ function handle(number,list){
             getGeneric(i);
         }
 
-        getGraphingData();
+       // getGraphingData();
+       Drawgraph();
         getAndDrawOverAllRating();
 
 });
